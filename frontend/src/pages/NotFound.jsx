6e4c754334398633b6065f4e8f5758f56a02150a
ovita-angera <ovita.brian@gmail.com>
2024-03-30 14:08:18 +0300

    updates

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,7 +1,10 @@
+import { FaTriangleExclamation } from 'react-icons/fa6'
+
 function NotFound() {
     return <div>
+        <FaTriangleExclamation style={{height: "200px", width: "200px", color: "red"}} />
         <h1>404 Not Found</h1>
-        <p>The page you're looking for doesn't exist!</p>
+        <p>The page you are looking for does not exist!</p>
     </div>
 }
 
